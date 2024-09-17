9ae19c74a2a3b69335ba826ba2cd9c86268e0405
Yongkang Chen <yonc@microsoft.com>
2024-09-17 12:33:45 +0800

    implement trpc client and query client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,27 +3,25 @@
 import type { NextPage } from 'next';
 import type { AppType, AppProps } from 'next/app';
 import type { ReactElement, ReactNode } from 'react';
+import type { Metadata } from 'next';
 
 import { DefaultLayout } from '~/components/DefaultLayout';
 import { trpc } from '~/utils/trpc';
 import '~/styles/globals.css';
 
-export type NextPageWithLayout<
-  TProps = Record<string, unknown>,
-  TInitialProps = TProps,
-> = NextPage<TProps, TInitialProps> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
+export const metadata: Metadata = {
+    title: 'Home',
+    description: 'Welcome to Next.js',
+}
 
-type AppPropsWithLayout = AppProps & {
-  Component2: NextPageWithLayout;
-};
-
-const MyApp = (({ Component2, pageProps }: AppPropsWithLayout) => {
-  const getLayout =
-    Component2.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>);
-
-  return getLayout(<Component2 {...pageProps} />);
-}) as AppType;
-
-export default trpc.withTRPC(MyApp);
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    )
+}
